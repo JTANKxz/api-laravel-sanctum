f7cc5cc40578b713e28cb308f198922e4dc886bd
joetankster <joetankster@gmail.com>
2025-12-15 03:53:44 +0000

    Atualização

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\SupportApiController;
+use App\Http\Controllers\SupportApiController;
 use App\Http\Controllers\AppConfigController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
